343a00c407b90d3f5d5cf0aff800679f2dad03e1
Olha Art <global14072012@gmail.com>
2024-06-21 14:57:42 +0200

    s

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,18 +1,17 @@
-import { MagnifyingGlass } from "react-loader-spinner";
+import { Hearts } from "react-loader-spinner";
 import css from "./Loader.module.css";
 
 export default function Loader() {
   return (
     <div className={css.loader}>
-      <MagnifyingGlass
-        visible={true}
+      <Hearts
         height="80"
         width="80"
-        ariaLabel="magnifying-glass-loading"
+        color="rgb(89, 24, 77)"
+        ariaLabel="hearts-loading"
         wrapperStyle={{}}
-        wrapperClass="magnifying-glass-wrapper"
-        glassColor="#c0efff"
-        color="#e15b64"
+        wrapperClass=""
+        visible={true}
       />
     </div>
   );
